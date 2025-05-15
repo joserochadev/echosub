87878157a3cbae4785c0b469fee61c0c73b4a2b6
José Rocha <joserochadev@gmail.com>
2025-05-15 14:15:47 -0300

    refac: add return type annotation and improve logging in ExtractAudioService

--- src/services/extract_audio_service.py
+++ src/services/extract_audio_service.py
@@ -6,14 +6,20 @@ from ..model.video import Video
 
 
 class ExtractAudioService:
-    def execute(video: Video):
+    def execute(video: Video) -> Path:
         audio_output_path = Path(f"output/{video.name}.mp3")
         audio_output_path.parent.mkdir(parents=True, exist_ok=True)
 
         try:
+            print("echosub - Extracting audio from video \n")
+
             video = VideoFileClip(video.path)
             audio = video.audio
             audio.write_audiofile(str(audio_output_path))
+
+            print("echosub - Audio extracted successfully! \n")
+
+            return audio_output_path
 
         except Exception as e:
             raise Exception(f"Error: Unable to extract audio: {e}")
