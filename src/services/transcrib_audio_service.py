df02195b91a86370a35e288aca7b0d6aeb9be9c5
José Rocha <joserochadev@gmail.com>
2025-06-08 01:58:51 -0300

    feat: integrate faster-whisper for audio transcription
    - Added faster-whisper as a dependency in pyproject.toml.
    - Replaced the whisper model with faster-whisper in TranscribAudioService.
    - Updated the transcription logic to handle segments and word timestamps.
    - Improved error handling and logging during the transcription process.

--- src/services/transcrib_audio_service.py
+++ src/services/transcrib_audio_service.py
@@ -1,18 +1,32 @@
 from pathlib import Path
 
-import whisper
+from faster_whisper import WhisperModel
 
 
 class TranscribAudioService:
     def execute(audio_path: Path, whisper_model: str) -> dict[str, str | list]:
-        model = whisper.load_model(whisper_model)
+        model = WhisperModel(whisper_model, device="cpu", compute_type="int8")
         print(f"echosub - whisper model {whisper_model} loaded!")
+
+        transcript_result = {"segments": []}
 
         try:
             print("echosub - Starting trascription")
-            transcript = model.transcribe(
-                str(audio_path), verbose=False, word_timestamps=True
+            segments, transcript_info = model.transcribe(
+                str(audio_path), word_timestamps=True
             )
+
+            for segment in segments:
+                print(f"[{segment.start:.2f}s -> {segment.end:.2f}s]: {segment.text}")
+
+                segment_dict = {
+                    "start": segment.start,
+                    "end": segment.end,
+                    "text": segment.text,
+                    "words": [word._asdict() for word in segment.words],
+                }
+
+                transcript_result["segments"].append(segment_dict)
 
             print("echosub - Transcription generated successfully! \n")
 
@@ -21,9 +35,4 @@ class TranscribAudioService:
         except Exception as e:
             raise Exception(f"Error: Unable to generate trascription \n {e}")
 
-        # for segment in transcript["segments"]:
-        #     print(
-        #         f"[{segment['start']:.2f}s -> {segment['end']:.2f}s]: {segment['text']}"
-        #     )
-
-        return transcript
+        return transcript_result
