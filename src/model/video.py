8c6bfbaa876dac502a1172e83a3a69905409294a
José Rocha <joserochadev@gmail.com>
2025-06-07 22:08:19 -0300

    feat: add video duration calculation in Video class

--- src/model/video.py
+++ src/model/video.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from moviepy import VideoFileClip
+
 
 class Video:
     def __init__(self, video_path: Path):
@@ -7,8 +9,10 @@ class Video:
         self.name = video_path.stem
         self.format = video_path.suffix
         self.size = video_path.stat().st_size  # Size in bytes
+        self.duration = None
 
         self.__is_valid_format()
+        self.__calc_video_duration()
 
     def __is_valid_format(self) -> bool:
         valid_formats = [".mp4", ".avi", ".mov", ".mkv"]
@@ -17,3 +21,10 @@ class Video:
             raise ValueError(f"Invalid video format: {self.format}")
 
         return True
+
+    def __calc_video_duration(self):
+        try:
+            with VideoFileClip(self.path) as video_clip:
+                self.duration = video_clip.duration
+        except Exception as e:
+            raise IOError(f"Could not read video file to get duration: {e}")
